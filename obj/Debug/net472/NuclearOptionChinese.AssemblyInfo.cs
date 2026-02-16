73df3d3c3eaf078fbf2bef559fb34ccea4e5506a
HunterCHCL <159284508+HunterCHCL@users.noreply.github.com>
2026-02-17 00:31:17 +0800

    更新程序集信息，修改AssemblyInformationalVersion和AssemblyReference缓存

--- obj/Debug/net472/NuclearOptionChinese.AssemblyInfo.cs
+++ obj/Debug/net472/NuclearOptionChinese.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NuclearOptionChinese")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c237085f9e59c8e0c3a7beba5dbb7f3ca305f3bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e766c942eaf4ca62a35c6ebb26fedf13d6de5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nuclear Option Chinese Translation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NuclearOptionChinese")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
